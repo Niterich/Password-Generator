35094dc70a4ab3584212da7ea2f305767705a3d4
Niterich <52295078+Niterich@users.noreply.github.com>
2019-11-24 18:02:27 -0700

    finished generator logic

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,33 +10,67 @@
 
 console.log("connected!");
 
-let userChoices
-    
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", ",", ".", ":", ";", "-", "_", "+", "="]
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//Declare the variables i will use
+let userChoices = [];
+let randomPassword = "";
+let arrayLC = [];
+let arrayUC = [];
+let arraySC = [];
+let arrayNum = [];
+let randomIndexNumber = 0;
 
-const passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.")
-if (passwordLength < 7 && passwordLength < 129) {
+//Declare the arrys with the various character options    
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", ",", ".", ":", ";", "-", "_", "+", "="]
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-} else {
+//Prompt user for a password length and dont let them proceed until a valid number is chosen
+let passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.")
+while (passwordLength <= 7 || passwordLength >= 129) {
     alert("Please select a number between 8 and 128");
-}
+    passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.");
+    console.log(passwordLength);
+} 
+
+console.log(passwordLength);
 
 const userLC = confirm("Do you want lower case letters in your password?");
+console.log(userLC);
 const userUC = confirm("Do you want upper case letters in your password?");
+console.log(userUC);
 const userSC = confirm("Do you want special characters in your password?");
+console.log(userSC);
 const userNum = confirm("Do you want numbers in your password?");
+console.log(userNum);
 
+if (userLC) {
+    arrayLC = userChoices.concat(lowerCase);
+    userChoices = arrayLC;
+    console.log(arrayLC);
+};
+if (userUC) {
+    arrayUC = userChoices.concat(upperCase);
+    userChoices = arrayUC;
+    console.log(userChoices);
+};
+if (userSC) {
+    arraySC = userChoices.concat(specialCharacters);
+    userChoices = arraySC;
+    console.log(userChoices);
+};
+if (userSC) {
+    arrayNum = userChoices.concat(numbers);
+    userChoices = arrayNum;
+    console.log(userChoices);
+};
 
+console.log(userChoices);
 
+for (let i = 0; i < passwordLength; i++) {
+    randomIndexNumber = Math.floor(Math.random()*userChoices.length);
+    randomPassword += userChoices[randomIndexNumber];
+    console.log(randomIndexNumber);
+}
 
-
-// function randomStr(len, arr) { 
-//     var ans = ''; 
-//     for (var i = len; i > 0; i--) { 
-//         ans += arr[Math.floor(Math.random() * arr.length)]; 
-//     } 
-//     return ans; 
-// }
+console.log(randomPassword);
