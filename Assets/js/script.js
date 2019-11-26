c2b204dbaa83dd240834955c1e2fdc374b6eaf6e
Niterich <52295078+Niterich@users.noreply.github.com>
2019-11-25 19:01:32 -0700

    finished the logic and should have made the app unbreakable.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,14 @@
-// click gen password
-// prompt "how many chars" (8-128)
-    // if not, stop prompts
-// prompt "do you want lowercase, special chars, uppercase, numbers"
-    // must select at least 1 of these or it will stop prompts
-// display random password with all selected parameters
-// can copy to clipboard
-
-// will use arrays, objects, and if statements.
-
+// click gen password.
+// prompt "how many chars" (8-128).
+    // if not, keep asking until a valid number is entered.
+// prompt "do you want lowercase, special chars, uppercase, numbers".
+    // must select at least 1 of these or will loop until a valid option is chosen.
+// display random password with all selected parameters.
+// can copy to clipboard.
 
 console.log("connected!");
 
 //Declare the variables that will be used
-
 let userChoices = [];
 let randomPassword = "";
 let arrayLC = [];
@@ -20,6 +16,12 @@ let arrayUC = [];
 let arraySC = [];
 let arrayNum = [];
 let randomIndexNumber = 0;
+let passwordNumber = 0;
+let passwordLength = 0;
+let userLC;
+let userUC;
+let userSC;
+let userNum;
 
 //Declare the arrys with the various character options    
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -27,34 +29,64 @@ const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", ",", ".", ":", ";", "-", "_", "+", "="];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+//Function Declarations
+
 //Function to start the password generator that will run when the "Generate Password" button is clicked.
 function beginGenerator() {
-    //Prompt user for a password length and dont let them proceed until a valid number is chosen
-    let passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.");
-    // while (passwordLength <= 7 || passwordLength >= 129 || passwordLength != Number){
-    //     alert("Please select a number between 8 and 128");
-    //     passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.");
-    //     console.log(passwordLength);
-    // } 
-    while (passwordLength <= 7 || passwordLength >= 129){
+    choosePasswordLength();
+    console.log(passwordLength);
+    passwordSpecs();
+    while ((userLC === false) && (userUC === false) && (userSC === false) && (userNum === false)) {
+        alert("You must select at least one character type!");
+        passwordSpecs();
+    };
+    createMasterCharacterArray();
+    reset();
+};
+
+//Function to determine password length
+function choosePasswordLength() {
+    passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.");
+    
+    console.log(typeof passwordLength);
+    passwordNumber = parseInt(passwordLength);
+    passwordLength = passwordNumber;
+    console.log(typeof passwordLength);
+
+    while ((typeof passwordLength === "string") || (isNaN(passwordLength)) || (passwordLength <= 7 || passwordLength >= 129)){
         alert("Please select a number between 8 and 128");
         passwordLength = prompt("How long would you like your password to be? Please input a number between 8 and 128.");
-        console.log(passwordLength);
-    } 
+        passwordNumber = parseInt(passwordLength);
+        passwordLength = passwordNumber;
+        console.log(typeof passwordLength);
+    };
+};
 
-    console.log(passwordLength);
-
-    //Prompt user for password specifications
-    const userLC = confirm("Do you want lower case letters in your password?");
+//Function to choose password specifications
+function passwordSpecs() {
+    userLC = confirm("Do you want lower case letters in your password?");
     console.log(userLC);
-    const userUC = confirm("Do you want upper case letters in your password?");
+    userUC = confirm("Do you want upper case letters in your password?");
     console.log(userUC);
-    const userSC = confirm("Do you want special characters in your password?");
+    userSC = confirm("Do you want special characters in your password?");
     console.log(userSC);
-    const userNum = confirm("Do you want numbers in your password?");
+    userNum = confirm("Do you want numbers in your password?");
     console.log(userNum);
-    
-    
+};
+
+//Function to reset all variables
+function reset() {
+    userChoices = [];
+    randomPassword = "";
+    arrayLC = [];
+    arrayUC = [];
+    arraySC = [];
+    arrayNum = [];
+    randomIndexNumber = 0;
+};
+
+//Function to combine arrays based on user input, loop through the array, create a random password, and paste it to the text input
+function createMasterCharacterArray (){
     //Find which character choices the user picked and add them all together into one string.
     if (userLC) {
         arrayLC = userChoices.concat(lowerCase);
@@ -76,32 +108,12 @@ function beginGenerator() {
         userChoices = arrayNum;
         console.log(userChoices);
     };
-    // if (userChoices === []) {
-    //     alert("You must choose at least one character type!")
-    // }
-    
     //Generate a random number and loop through the master character array, adding each character at the corresponding randomly generated index number to the randomPassword string.
     for (let i = 0; i < passwordLength; i++) {
         randomIndexNumber = Math.floor(Math.random()*userChoices.length);
         randomPassword += userChoices[randomIndexNumber];
     };
-    
     console.log(randomPassword);
     // Paste random password to the text input.
-    
-    reset();
-
-    //Return the password.
-    // return randomPassword;
-};
-
-//Function to reset all variables
-function reset() {
-    userChoices = [];
-    randomPassword = "";
-    arrayLC = [];
-    arrayUC = [];
-    arraySC = [];
-    arrayNum = [];
-    randomIndexNumber = 0;
+    document.getElementById("pass").value = randomPassword;
 };
