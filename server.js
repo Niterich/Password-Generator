f191c55bf6849f88c07d5c827226fb302b44e286
Ryan <ryandub9@gmail.com>
2020-05-10 11:59:04 -0700

    adding heroku buildpack

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname + '/Assets'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
